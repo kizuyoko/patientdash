5fc769bffd438260eae18f66ba70dbcc9530396d
Yoko Kizu <kizuyoko@gmail.com>
2026-02-02 15:27:37 +0100

    Small style

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import PatientTable from './components/PatientTable';
 import Sidebar from './components/Sidebar';
 import Footer from './components/Footer';
 import Heading from './components/ui/Heading';
+import Paragraph from './components/ui/Paragraph';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Sidebar />
       <main>
         <Heading>Patient List</Heading>
+        <Paragraph>Overview of all registered patients</Paragraph>
         <PatientTable />
         <Footer />
       </main>
