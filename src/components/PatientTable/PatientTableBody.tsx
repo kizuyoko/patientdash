a7c78f654536872e824a766e60e90cf078789c8a
Yoko Kizu <kizuyoko@gmail.com>
2026-02-06 17:50:34 +0100

    PatientTableRow

--- src/components/PatientTable/PatientTableBody.tsx
+++ src/components/PatientTable/PatientTableBody.tsx
@@ -1,48 +1,17 @@
-import StatusBadge from "../ui/StatusBadge";
+import PatientTableRow from "./PatientTableRow";
+import { patients } from "../../data/fake_patients";
 
 const PatientTableBody = () => {
     return (
         <tbody>
-            <tr>
-              <td>PT-001</td>
-              <td><b>Sarah Johnson</b></td>
-              <td>34</td>
-              <td>
-                <StatusBadge status="active" />
-              </td>
-              <td>Jan 27, 2026</td>
-              <td>Hypertension</td>
-            </tr>
-            <tr>
-              <td>PT-002</td>
-              <td><b>Michael Chen</b></td>
-              <td>45</td>
-              <td>
-                <StatusBadge status="completed" />
-              </td>
-              <td>Jan 25, 2026</td>
-              <td>Diabetes Type 2</td>
-            </tr>
-            <tr>
-              <td>PT-003</td>
-              <td><b>Emily Rodriguez</b></td>
-              <td>28</td>
-              <td>
-                <StatusBadge status="active" />
-              </td>
-              <td>Jan 8, 2026</td>
-              <td>Asthma</td>
-            </tr>  
-            <tr>
-              <td>PT-004</td>
-              <td><b>James Williams</b></td>
-              <td>52</td>
-              <td>
-                <StatusBadge status="pending" />
-              </td>
-              <td>Jan 20, 2026</td>
-              <td>Heart Disease</td>
-            </tr>           
+            {
+              patients.map((patient) => (
+                <PatientTableRow 
+                  key={patient.id} 
+                  patient={patient} 
+                />
+              ))
+            }
         </tbody>
     );
 };
---
+++ src/components/PatientTable/PatientTableRow.tsx
@@ -0,0 +1,23 @@
+import StatusBadge from "../ui/StatusBadge";
+import type { Patient } from "../../types/patient";
+
+type PatientTableRowProps = {
+    patient: Patient
+};
+
+const PatientTableRow = ({patient}: PatientTableRowProps) => {
+    return (
+        <tr>
+            <td>{patient.id}</td>
+            <td><b>{patient.name}</b></td>
+            <td>{patient.age}</td>
+            <td>
+            <StatusBadge status={patient.status} />
+            </td>
+            <td>{patient.lastVisit}</td>
+            <td>{patient.condition}</td>
+        </tr>
+    );
+};
+
+export default PatientTableRow;
